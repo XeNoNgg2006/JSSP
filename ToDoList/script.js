d42ea17a047bb99bc98cf159d5187f81045f7e8a
xenon <constantinbarba2006@gmail.com>
2024-11-15 00:17:22 +0100

    finished everything except theme change

--- ToDoList/script.js
+++ ToDoList/script.js
@@ -1,10 +1,14 @@
 const input = document.getElementById("input-task");
 const addButton = document.getElementById("button-task");
-const itemList = document.getElementById("ul-task")
+const taskList = document.getElementById("ul-task")
 
 const lightButton =document.getElementById("light");
 const darkButton = document.getElementById("dark");
 const colurfulButton = document.getElementById("colorful");
+
+
+
+
 
 function addTask(){
     console.log("Button clicked!");
@@ -14,16 +18,63 @@ function addTask(){
         alert("Please enter an Task.")
         return;
     }
+    const newCheckbox = document.createElement("input");
+    newCheckbox.type = "checkbox";
+    newCheckbox.className = "checkbox-input";
+    newCheckbox.id = "checkbox-input";
+    
+    const deleteItem = document.createElement("input");
+    deleteItem.type = "button";
+    deleteItem.className = "delete-input";
+    deleteItem.id = "delete-input";
 
-    const listItem = document.createElement("li");
-    listItem.textContent = itemText;
 
-    itemList.appendChild(listItem);
+    const taskItem = document.createElement("li");
+    
+    taskItem.append(newCheckbox, document.createTextNode(itemText), deleteItem );
+    
 
+    taskList.appendChild(taskItem);
+    
     input.value = "";
 }
 
+
 addButton.addEventListener("click", addTask);
+
+
+taskList.addEventListener('click', function(event) {
+     
+    if (event.target && event.target.classList.contains('delete-input')) {
+        const taskItem = event.target.closest("li");
+       
+        if(taskItem) {
+           
+            taskItem.remove();
+        }
+
+     }
+
+    if (event.target && event.target.classList.contains('checkbox-input')) {
+        
+        const taskText = event.target.closest('li')
+        
+        
+        if (taskText) {
+            if (event.target.checked) {
+                taskText.style.textDecoration = 'line-through';
+                taskText.style.color = "#808080";
+                taskText.style.textDecorationColor = "#808080";
+                taskText.style.textDecorationThickness = "2px";
+            } else {
+                taskText.style.textDecoration = 'none'; // Remove strike-through
+                taskText.style.color = "#e0e0e0";
+                
+            }
+        }
+    }
+});
+
 
 
 input.addEventListener("keypress", function(event){
