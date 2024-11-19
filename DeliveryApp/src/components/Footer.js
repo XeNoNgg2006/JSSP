00752793e95942f861caa8bfc06da723592cecce
xenon <constantinbarba2006@gmail.com>
2024-11-19 23:44:29 +0100

    htwg with yaury 19/11

---
+++ DeliveryApp/src/components/Footer.js
@@ -0,0 +1,11 @@
+import "./styles/footer.css"
+
+function Footer() {
+    return(
+        <div className="footer-div">
+            <h1>Footer</h1>
+        </div>
+    );
+}
+
+export default Footer
