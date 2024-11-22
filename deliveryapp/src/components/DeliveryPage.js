818a3b3509e0011733f8f54d21e2f8549a7da653
xenon <constantinbarba2006@gmail.com>
2024-11-22 20:05:48 +0100

    some part of delivery page

--- deliveryapp/src/components/DeliveryPage.js
+++ deliveryapp/src/components/DeliveryPage.js
@@ -6,8 +6,19 @@ function DeliveryPage() {
     return(
         <div>
         <Header/>
-        <main className="delivery__main">
-            <h1>f</h1>
+        <main className="dp_main">
+            <button className="back-button">&lt; Back</button>
+            <div className="dp_address-window">
+            
+                <h1 className="dp_adress-window__header">Delivery Adress</h1>
+                <form className="dp_form-window">
+                    <input placeholder="Postal Code*" className="dp_adress postal-code"/>
+                    <input placeholder="Street*" className="dp_adress street"/>
+                    <input placeholder="Street Number*" className="dp_adress street-number"/>
+                </form>
+                <button type="button" className="dp_submit-adress">Add Adress</button>
+            </div>
+
         </main>
         <Footer/>
         </div>
